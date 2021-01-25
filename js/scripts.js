f4c0cc419631b5d87876cd3e13ab4b2f3cf71dfe
kingwar66 <kingwar66@gmail.com>
2021-01-25 17:42:08 +0200

    add scripts for getting data from the API

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,43 @@
 'use strict'
 
-const key = 'https://geo.ipify.org/api/v1?apiKey=at_Hlx4pvDR7vRvSvhFkyiW25Hi7aTqp&ipAddress=8.8.8.8'
+//ipify key
+const key = 'at_Hlx4pvDR7vRvSvhFkyiW25Hi7aTqp'
+const url = `https://geo.ipify.org/api/v1?apiKey=${key}`
+
+// appending the ip data to the DOM
+const appendToDom = (data) => {
+    //output the data of [ip-address, region, timezone, isp]
+    $('#ip-address').text(data.ip);
+    $('#region').text(`${data.location.country} , ${data.location.region}`);
+    $('#timezone').text('UTC ' + data.location.timezone);
+    $('#isp').text(data.as.name);
+}
+
+//fetching ip function
+const fetchIp = async (inputVal) => {
+    try {
+        let res;
+        if (inputVal) {
+            res = await axios.get(`${url}&ipAddress=${inputVal}`);
+        } else {
+            res = await axios.get(url);
+        }
+        const data = res.data
+        //append to Dom
+        appendToDom(data)
+        console.log(data)
+
+    } catch (err) {
+        console.log(err)
+    }
+}
+
+//get input ip val on btn click
+$('#inputBtn').click(function () {
+    const inputVal = $('#ip-value').val();
+    //pass value into ip fun
+    fetchIp(inputVal);
+});
+
+//init ip Function
+fetchIp();
