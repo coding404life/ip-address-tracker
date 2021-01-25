0adcc5bb4fbf9586d0863580654609591de5ebea
kingwar66 <kingwar66@gmail.com>
2021-01-25 21:25:42 +0200

    leaflet map works on typing new ip

--- dist/js/all.js
+++ dist/js/all.js
@@ -12,6 +12,25 @@ const appendToDom = (data) => {
     $('#timezone').text('UTC ' + data.location.timezone);
     $('#isp').text(data.as.name);
 }
+//map
+const map = (lat, lng) => {
+    //remove map if exist
+    $('#map #mapid').remove();
+    $('#map').append('<div id=mapid></div>');
+
+    let mymap = L.map('mapid').setView([lat, lng], 15);
+    //map tiler
+    L.tileLayer('https://api.maptiler.com/maps/streets/{z}/{x}/{y}.png?key=N1G66AEsmvdXauZnxmxV', {
+        attribution: '<a href="https://www.maptiler.com/copyright/" target="_blank">&copy; MapTiler</a> <a href="https://www.openstreetmap.org/copyright" target="_blank">&copy; OpenStreetMap contributors</a>',
+        maxZoom: 18,
+        id: 'mapbox/streets-v11',
+        tileSize: 512,
+        zoomOffset: -1,
+        accessToken: 'your.mapbox.access.token'
+    }).addTo(mymap);
+    //map marker
+    L.marker([lat, lng]).addTo(mymap);
+}
 
 //fetching ip function
 const fetchIp = async (inputVal) => {
@@ -22,9 +41,13 @@ const fetchIp = async (inputVal) => {
         } else {
             res = await axios.get(url);
         }
+        //1. API data
         const data = res.data
-        //append to Dom
+        //2. append to Dom
         appendToDom(data)
+        //3. passing lat and lng to the map 
+        map(data.location.lat, data.location.lng);
+
         console.log(data)
 
     } catch (err) {
@@ -36,11 +59,8 @@ const fetchIp = async (inputVal) => {
 $('#inputBtn').click(function () {
     const inputVal = $('#ip-value').val();
     //pass value into ip fun
-    fetchIp(inputVal);
+    fetchIp(inputVal)
 });
 
 //init ip Function
 fetchIp();
-
-//init map 
-var mymap = L.map('mapid').setView([51.505, -0.09], 13);
